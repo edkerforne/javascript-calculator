63f5af72c936aea16f2631a1c50f2c84a306d6d1
Edwin Kerforne <edkerforne@users.noreply.github.com>
2020-02-05 02:43:00 +0100

    Updates

---
+++ precache-manifest.a98932b667734661f104d73edce5560f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96e8d3bd905840f268ef3380942b9fd",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "7e6a77b6356da7cfcf8f",
+    "url": "/javascript-calculator/static/css/main.7805f38c.chunk.css"
+  },
+  {
+    "revision": "ce92c84967d3f8ac4c9f",
+    "url": "/javascript-calculator/static/js/2.c0fffe79.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/javascript-calculator/static/js/2.c0fffe79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6a77b6356da7cfcf8f",
+    "url": "/javascript-calculator/static/js/main.b16946b0.chunk.js"
+  },
+  {
+    "revision": "054eac1b9e1be10a5c81",
+    "url": "/javascript-calculator/static/js/runtime-main.aafb8b0a.js"
+  }
+]);
